a8f1c8646a4b5e41d5f12bfc7763691c1799cd4f
18435113582 <18435113582@163.com>
2018-05-11 17:32:17 +0800

    萨达姆第一次

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -1,92 +1,223 @@
 <!DOCTYPE html>
-<html>
- <head>
-        <title>@yield('title')</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--><html lang="en"><!--<![endif]-->
+<head>
+<meta charset="utf-8">
 
-        <link rel="stylesheet" href="/admins/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/admins/css/bootstrap-theme.min.css">
-        <!-- <script type="text/javascript" src="/admins/js/jquery.js"></script> -->
-        <!-- <script type="text/javascript" src="/admins/js/bootstrap.min.js"></script> -->
-        <script type="text/javascript" src="/admins/js/jquery-1.8.3.min.js"></script>
+<!-- Viewport Metatag -->
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+<!-- Plugin Stylesheets first to ease overrides -->
+<link rel="stylesheet" type="text/css" href="/admins/plugins/colorpicker/colorpicker.css" media="screen">
+
+<!-- Required Stylesheets -->
+<link rel="stylesheet" type="text/css" href="/admins/bootstrap/css/bootstrap.min.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/css/fonts/ptsans/stylesheet.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/css/fonts/icomoon/style.css" media="screen">
+
+<link rel="stylesheet" type="text/css" href="/admins/css/mws-style.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/css/icons/icol16.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/css/icons/icol32.css" media="screen">
+
+<!-- Demo Stylesheet -->
+<link rel="stylesheet" type="text/css" href="/admins/css/demo.css" media="screen">
+
+<!-- jQuery-UI Stylesheet -->
+<link rel="stylesheet" type="text/css" href="/admins/jui/css/jquery.ui.all.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/jui/jquery-ui.custom.css" media="screen">
+
+<!-- Theme Stylesheet -->
+<link rel="stylesheet" type="text/css" href="/admins/css/mws-theme.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/css/themer.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/admins/css/admins.css" media="screen">
+
+<title>@yield('title')</title>
+
+</head>
+
+<body>
+
+    <!-- Header -->
+    <div id="mws-header" class="clearfix">
+    
+        <!-- Logo Container -->
+        <div id="mws-logo-container">
         
-        
-        <!-- jQuery AND jQueryUI -->
-        <script type="text/javascript" src="/admins/js/libs/jquery/1.6/jquery.min.js"></script>
-        <script type="text/javascript" src="/admins/js/libs/jqueryui/1.8.13/jquery-ui.min.js"></script>
-
-        <link rel="stylesheet" href="/admins/css/min.css" />
-        <script type="text/javascript" src="/admins/js/min.js"></script>
-        
-    </head>
-    <body>
-        
-        <script type="text/javascript" src="/admins/content/settings/main.js"></script>
-        <!-- <link rel="stylesheet" href="content/settings/style.css" /> -->
-
-
-
-        <!--              
-                HEAD
-                        --> 
-        <div id="head" style="background:#494949">
-            <div class="left">
-                <a href="#" class="button profile"><img src="/admins/img/huser.png" alt="" /></a>
-                Hi, 
-                <a href="#">用户名</a>
-                |
-                <a href="#">退出</a>
-            </div>
-            <div class="right">
-                <form action="#" id="search" class="search placeholder">
-                    <label>Looking for something ?</label>
-                    <input type="text" value="" name="q" class="text"/>
-                    <input type="submit" value="rechercher" class="submit"/>
-                </form>
+            <!-- Logo Wrapper, images put within this wrapper will always be vertically centered -->
+            <div id="mws-logo-wrap">
+                <span style="color:white;font-size:20px;">lamp201</span>
             </div>
         </div>
+        
+        <!-- User Tools (notifications, logout, profile, change password) -->
+        <div id="mws-user-tools" class="clearfix">
+        
+            <!-- Notifications -->
+            <div id="mws-user-notif" class="mws-dropdown-menu">
+                <a href="#" data-toggle="dropdown" class="mws-dropdown-trigger"><i class="icon-exclamation-sign"></i></a>
                 
+                <!-- Unread notification count -->
+                <span class="mws-dropdown-notif">35</span>
                 
-        <!--            
-                SIDEBAR
-                         --> 
-        <div id="sidebar">
-            <ul>
-                <li>
-                        <a href="#">后台模板</a>
-                </li>
-                <li class="current"><a href="#"><img src="/admins/img/layout.png" alt="" /> Elements</a>
+                <!-- Notifications dropdown -->
+                <div class="mws-dropdown-box">
+                    <div class="mws-dropdown-content">
+                        <ul class="mws-notifications">
+                            <li class="read">
+                                <a href="#">
+                                    <span class="message">
+                                        Lorem ipsum dolor sit amet consectetur adipiscing elit, et al commore
+                                    </span>
+                                    <span class="time">
+                                        January 21, 2012
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="mws-dropdown-viewall">
+                            <a href="#">View All Notifications</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Messages -->
+            <div id="mws-user-message" class="mws-dropdown-menu">
+                <a href="#" data-toggle="dropdown" class="mws-dropdown-trigger"><i class="icon-envelope"></i></a>
+                
+                <!-- Unred messages count -->
+                <span class="mws-dropdown-notif">35</span>
+                
+                <!-- Messages dropdown -->
+                <div class="mws-dropdown-box">
+                    <div class="mws-dropdown-content">
+                        <ul class="mws-messages">
+                            <li class="read">
+                                <a href="#">
+                                    <span class="sender">John Doe</span>
+                                    <span class="message">
+                                        Lorem ipsum dolor sit amet consectetur adipiscing elit, et al commore
+                                    </span>
+                                    <span class="time">
+                                        January 21, 2012
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="mws-dropdown-viewall">
+                            <a href="#">View All Messages</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- User Information and functions section -->
+            <div id="mws-user-info" class="mws-inset">
+            
+                <!-- User Photo -->
+                <div id="mws-user-photo">
+                    <img src="/admins/example/profile.jpg" alt="User Photo">
+                </div>
+                
+                <!-- Username and Functions -->
+                <div id="mws-user-functions">
+                    <div id="mws-username">
+                        Hello, John Doe
+                    </div>
                     <ul>
-	                    <li class="current"><a href="dashboard.html?p=index">Dashboard</a></li>
-	                    <li><a href="forms.html?p=forms">Forms</a></li>
-	                    <li><a href="table.html?p=table">Table</a></li>
-	                    <li><a href="tabs.html?p=tabs">Tabs</a></li>
-	                    <li><a href="gallery.html?p=gallery">Gallery</a></li>
-	                    <li><a href="notifications.html?p=notif">Notifications</a></li>
-	                    <li><a href="charts.html?p=charts">Charts</a></li>
-	                    <li><a href="typography.html?p=typo">Typography</a></li>
-	                    <li><a href="icons.html?p=icons">Icons</a></li>
-	                    <li><a href="calendar.html?p=calendar">Calendar</a></li>
-                	</ul>
-                </li>
-            </ul>
-
-
-        </div>  
-                
-        <!--            
-              CONTENT 
-                        --> 
-        <div id="content" class="white">
-		
-		@section('content')
-
-
-
-		@show
-
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Change Password</a></li>
+                        <li><a href="index.html">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Start Main Wrapper -->
+    <div id="mws-wrapper">
+    
+        <!-- Necessary markup, do not remove -->
+        <div id="mws-sidebar-stitch"></div>
+        <div id="mws-sidebar-bg"></div>
+        
+        <!-- Sidebar Wrapper -->
+        <div id="mws-sidebar">
+        
+            <!-- Hidden Nav Collapse Button -->
+            <div id="mws-nav-collapse">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
+            <!-- Searchbox -->
+            
+            
+            <!-- Main Navigation -->
+            <div id="mws-navigation">
+                <ul>
+                    <li>
+                        <a href="#"><i class="icon-list"></i> 用户管理</a>
+                        <ul>
+                            <li><a href="#">添加用户</a></li>
+                            <li><a href="#">用户列表</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
         
+        <!-- Main Container Start -->
+        <div id="mws-container" class="clearfix">
+            <div class="container">
+            @section('content')
+
+            @show
+            </div>     
+            <!-- Footer -->
+            <div id="mws-footer">
+                Copyright Your Website 2012. All Rights Reserved.
+            </div>
+            
+        </div>
+        <!-- Main Container End -->
         
-    </body>
+    </div>
+
+    <!-- JavaScript Plugins -->
+    <script src="/admins/js/libs/jquery-1.8.3.min.js"></script>
+    <script src="/admins/js/libs/jquery.mousewheel.min.js"></script>
+    <script src="/admins/js/libs/jquery.placeholder.min.js"></script>
+    <script src="/admins/custom-plugins/fileinput.js"></script>
+    
+    <!-- jQuery-UI Dependent Scripts -->
+    <script src="/admins/jui/js/jquery-ui-1.9.2.min.js"></script>
+    <script src="/admins/jui/jquery-ui.custom.min.js"></script>
+    <script src="/admins/jui/js/jquery.ui.touch-punch.js"></script>
+
+    <!-- Plugin Scripts -->
+    <script src="/admins/plugins/colorpicker/colorpicker-min.js"></script>
+
+    <!-- Core Script -->
+    <script src="/admins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/admins/js/core/mws.js"></script>
+
+    <!-- Themer Script (Remove if not needed) -->
+    <script src="/admins/js/core/themer.js"></script>
+
+    <!-- Demo Scripts (remove if not needed) -->
+    <script type="text/javascript">
+        $(function() {
+            $.fn.tabs && $('.mws-tabs').tabs();
+        });
+    </script>
+
+    @section('js')
+
+    @show
+
+</body>
 </html>
